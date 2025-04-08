9dd5a4ce4548a95520794b502582002a6ce5c28b
Eman Ali Hassan Moawad <eman.ali@flairstech.com>
2025-04-08 21:31:36 +0200

    enhance producer and add ProduceBookingRequestAsync , back yaml with UI issue

--- API/Producer/KafkaProducer.cs
+++ API/Producer/KafkaProducer.cs
@@ -1,29 +1,52 @@
-﻿using Confluent.Kafka;
+﻿using API.Models;
+using System.Text.Json;
+using Confluent.Kafka;
+using static Confluent.Kafka.ConfigPropertyNames;
 
 namespace KafkaWebApiDemo.Services
 {
-    public class KafkaProducerService
+    public class KafkaProducerService : IDisposable
     {
         private readonly IConfiguration _config;
-        private readonly string _bootstrapServers;
         private readonly string _topic;
+        private readonly IProducer<Null, string> _producer;
 
         public KafkaProducerService(IConfiguration config)
         {
             _config = config;
-            _bootstrapServers = _config["Kafka:BootstrapServers"];
+
             _topic = _config["Kafka:Topic"];
+            var producerConfig = new ProducerConfig
+            {
+                BootstrapServers = _config["Kafka:BootstrapServers"]
+            };
+            _producer = new ProducerBuilder<Null, string>(producerConfig).Build();
         }
 
         public async Task<string> SendMessageAsync(string message)
         {
-            var config = new ProducerConfig { BootstrapServers = _bootstrapServers };
-
-            using var producer = new ProducerBuilder<Null, string>(config).Build();// NULL msg key , string msg type 
-
-            var result = await producer.ProduceAsync(_topic, new Message<Null, string> { Value = message });
+            var result = await _producer.ProduceAsync(_topic, new Message<Null, string> { Value = message });
 
             return $"Sent to: {result.TopicPartitionOffset}";
+        }
+
+        public async Task ProduceBookingRequestAsync(TicketBooking booking)
+        {
+            try
+            {
+                var message = JsonSerializer.Serialize(booking);
+                await _producer.ProduceAsync(_topic, new Message<Null, string> { Value = message });
+                Console.WriteLine($"Produced booking request: {message}");
+            }
+            catch (ProduceException<Null, string> ex)
+            {
+                Console.WriteLine($"Delivery failed: {ex.Error.Reason}");
+            }
+        }
+        public void Dispose()
+        {
+            _producer.Flush(TimeSpan.FromSeconds(10));
+            _producer.Dispose();
         }
     }
 }
